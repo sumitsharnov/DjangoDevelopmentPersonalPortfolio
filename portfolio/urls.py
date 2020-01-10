f17d9e32a90a37c9efe29b7abbec58348045b434
sumitsharnov <sumit.kumar@gerogiancollege.ca>
2020-01-10 00:49:49 -0500

    Static image amd Media files

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -20,6 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', jobs.views.home, name='home'),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('admin/', admin.site.urls),
+    path('', jobs.views.home, name='home'),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
